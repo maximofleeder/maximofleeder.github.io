198131775c4760f205caa16d2c1cf915a1e3953d
Lukas Kindl <kindl@posteo.at>
2016-06-06 13:54:36 +0200

    Reduzierung von 3 js-Dateien auf 2.

--- brazil/brazil.js
+++ brazil/brazil.js
@@ -1,60 +1,109 @@
-/*
-	Zeigt die Reiseroute in Brasilien mit Photos von Panoramio und Wikipedia-Einträgen
-*/
-
 window.onload = function() {
 
-	var marker1 = L.marker([-23.5, -46.616667]).addTo(brazmap);
-		marker1.bindPopup('<h2>São Paulo</h2><p>Bem-vindo a São Paulo!</p>')
-	var marker2 = L.marker([-23.140556, -44.247222]).addTo(brazmap);
-		marker2.bindPopup('<h2>Ilha Grande</h2><p>Willkommen auf Ilha Grande!</p>')
-	var marker3 = L.marker([-22.908333, -43.196389]).addTo(brazmap);
-		marker3.bindPopup('<h2>Rio de Janeiro</h2><p>Bem-vindo ao Rio de Janeiro!</p>')
-	var marker4 = L.marker([-20.385278, -43.503333]).addTo(brazmap);
-		marker4.bindPopup('<h2>Minas Gerais</h2><p>Bem-vindo a Ouro Preto!</p>')
-	var marker5 = L.marker([-15.8, -47.85]).addTo(brazmap);
-		marker5.bindPopup('<h2>Distrito Federal</h2><p>Bem-vindo a Brasília!</p>')
-	var marker6 = L.marker([-8.758611, -63.881944]).addTo(brazmap);
-		marker6.bindPopup('<h2>Rondônia</h2><p>Bem-vindo ao Porto Velho!</br></br>Porto Velho ist die Hauptstadt des brasilianischen Bundesstaates Rondônia. Die Stadt hat rund 500.000 EinwohnerInnen.</p>')
-	var marker7 = L.marker([-10.951944, -61.951667]).addTo(brazmap);
-		marker7.bindPopup('<h2>Rondônia</h2><p>Hier ist Ji-Paraná</p>')
-	var marker8 = L.marker([-9.971111, -67.811111]).addTo(brazmap);
-		marker8.bindPopup('<h2>Acre</h2><p>Hier ist Rio Branco</p>')
-		
-	var markergroup = new L.featureGroup([marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8]);
-	brazmap.fitBounds(markergroup.getBounds());
-	
-	L.control.scale({
+    var brazmap = L.map('map_brazil');
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(brazmap);
+
+    var routing = L.Routing.control({
+        show: false,
+        waypoints: [
+            L.latLng(-23.5, -46.616667),
+            L.latLng(-23.140556, -44.247222),
+            L.latLng(-22.908333, -43.196389),
+            L.latLng(-20.385278, -43.503333),
+            L.latLng(-22.908333, -43.196389),
+            L.latLng(-15.8, -47.85),
+            L.latLng(-8.758611, -63.881944),
+            L.latLng(-10.951944, -61.951667),
+            L.latLng(-9.971111, -67.811111)
+        ]
+    }).addTo(brazmap);
+
+    // Variable für den Tipp Marker definieren
+    var tipp_marker;
+
+    // Routing control hinzufügen und minimieren
+    var routing_control = L.Routing.control({
+        show: false
+    }).addTo(brazmap);
+
+    // Klicks auf Karte verarbeiten
+    brazmap.on("click", function(event) {
+        if (tipp_marker) {
+            // Wegpunkte setzen und Routing control zeigen
+            routing_control.setWaypoints([
+                tipp_marker.getLatLng(),
+                event.latlng
+            ]);
+            routing_control.show();
+            // Tipp Marker löschen
+            brazmap.removeLayer(tipp_marker);
+            tipp_marker = null;
+        } else {
+            // Routing control minimieren
+            routing_control.hide()
+                // Tipp anzeigen und Marker merken
+            tipp_marker = L.marker(event.latlng).addTo(brazmap);
+            tipp_marker.bindPopup('Ziel klicken ...').openPopup();
+        }
+    });
+
+    /*
+    	Zeigt die Reiseroute in Brasilien mit Photos von Panoramio und Wikipedia-Einträgen
+    */
+
+    var marker1 = L.marker([-23.5, -46.616667]).addTo(brazmap);
+    marker1.bindPopup('<h2>São Paulo</h2><p>Bem-vindo a São Paulo!</p>')
+    var marker2 = L.marker([-23.140556, -44.247222]).addTo(brazmap);
+    marker2.bindPopup('<h2>Ilha Grande</h2><p>Willkommen auf Ilha Grande!</p>')
+    var marker3 = L.marker([-22.908333, -43.196389]).addTo(brazmap);
+    marker3.bindPopup('<h2>Rio de Janeiro</h2><p>Bem-vindo ao Rio de Janeiro!</p>')
+    var marker4 = L.marker([-20.385278, -43.503333]).addTo(brazmap);
+    marker4.bindPopup('<h2>Minas Gerais</h2><p>Bem-vindo a Ouro Preto!</p>')
+    var marker5 = L.marker([-15.8, -47.85]).addTo(brazmap);
+    marker5.bindPopup('<h2>Distrito Federal</h2><p>Bem-vindo a Brasília!</p>')
+    var marker6 = L.marker([-8.758611, -63.881944]).addTo(brazmap);
+    marker6.bindPopup('<h2>Rondônia</h2><p>Bem-vindo ao Porto Velho!</br></br>Porto Velho ist die Hauptstadt des brasilianischen Bundesstaates Rondônia. Die Stadt hat rund 500.000 EinwohnerInnen.</p>')
+    var marker7 = L.marker([-10.951944, -61.951667]).addTo(brazmap);
+    marker7.bindPopup('<h2>Rondônia</h2><p>Hier ist Ji-Paraná</p>')
+    var marker8 = L.marker([-9.971111, -67.811111]).addTo(brazmap);
+    marker8.bindPopup('<h2>Acre</h2><p>Hier ist Rio Branco</p>')
+
+    var markergroup = new L.featureGroup([marker1, marker2, marker3, marker4, marker5, marker6, marker7, marker8]);
+    brazmap.fitBounds(markergroup.getBounds());
+
+    L.control.scale({
         'metric': true
     }).addTo(brazmap);
-	
-	//Variable für ersten geklickten Punkt
-	var first_point;
-	
-	//Klicks auf Karte verarbeiten
-	brazmap.on("click", function (event) {
-		if (first_point) {
-			//zeichnen der Route
-			console.log("2. Punkt", event);
-			
-			routing_control.setWaypoints([
-				first_point,
-				event.latlng
-			])
-			routing_control.show();
-			
-			//ersten Punkt wieder löschen
-			first_point = null;
-		} else {
-			
-			routing_control.hide();
-			//merken des ersten Punkts
-			first_point = event.latlng;
-			console.log("1. Punkt", event);
-		}
-	});
-	
-	var bounds = markergroup.getBounds();
+
+    //Variable für ersten geklickten Punkt
+    var first_point;
+
+    //Klicks auf Karte verarbeiten
+    brazmap.on("click", function(event) {
+        if (first_point) {
+            //zeichnen der Route
+            console.log("2. Punkt", event);
+
+            routing_control.setWaypoints([
+                first_point,
+                event.latlng
+            ])
+            routing_control.show();
+
+            //ersten Punkt wieder löschen
+            first_point = null;
+        } else {
+
+            routing_control.hide();
+            //merken des ersten Punkts
+            first_point = event.latlng;
+            console.log("1. Punkt", event);
+        }
+    });
+
+    var bounds = markergroup.getBounds();
     var url = "http://www.panoramio.com/map/get_panoramas.php?set=public&from=0&to=20" +
         '&minx=' + bounds.getWest() +
         '&miny=' + bounds.getSouth() +
@@ -105,6 +154,6 @@ window.onload = function() {
             // console.log(wik_mark)
         }
     }
-	
-	var hash = new L.Hash(brazmap);
+
+    var hash = new L.Hash(brazmap);
 }
